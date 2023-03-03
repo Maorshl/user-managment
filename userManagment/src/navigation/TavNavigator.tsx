2a934d00cbdf7584427bad4193925e7a8230569a
Maor Shlomo <maors@moveo.co.il>
2023-03-03 08:57:07 +0200

    Created profile screen tab

--- userManagment/src/navigation/TavNavigator.tsx
+++ userManagment/src/navigation/TavNavigator.tsx
@@ -3,6 +3,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {TabsParamList} from '../contants/screens';
 import HomeScreen from '../features/home/HomeScreen';
+import ProfileScreen from '../features/profile/ProfileScreen';
 
 export default function TavNavigator() {
   const Tab = createBottomTabNavigator<TabsParamList>();
@@ -11,6 +12,7 @@ export default function TavNavigator() {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
