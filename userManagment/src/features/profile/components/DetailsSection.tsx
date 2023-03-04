cf1bcf3c806d659e5a0b090cd3d1df6de2f2aad0
Maor Shlomo <maors@moveo.co.il>
2023-03-04 19:59:24 +0200

    Added male and female boxes

--- userManagment/src/features/profile/components/DetailsSection.tsx
+++ userManagment/src/features/profile/components/DetailsSection.tsx
@@ -1,4 +1,4 @@
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {Image, ImageSourcePropType, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {colors} from '../../../constants/colors';
 import {useAppSelector} from '../../../store/store';
@@ -9,18 +9,71 @@ const DetailsSection = () => {
   const renderTextSection = ({
     value,
     name,
+    icon,
   }: {
     value: string;
     name: string;
-    icon?: JSX.Element;
+    icon?: ImageSourcePropType;
   }) => (
     <View style={styles.section}>
       <Text>{name}</Text>
       <View style={styles.value}>
+        {icon ? <Image source={icon} style={styles.icon} /> : null}
         <Text>{value}</Text>
       </View>
     </View>
   );
+
+  const renderGenderSection = () => (
+    <View style={styles.section}>
+      <Text>Gender</Text>
+      <View style={styles.genderBoxesContainer}>
+        {renderGenderBox(EGender.male)}
+        {renderGenderBox(EGender.female)}
+      </View>
+    </View>
+  );
+
+  enum EGender {
+    male = 'male',
+    female = 'female',
+  }
+
+  const genderDictionary: {
+    [key in EGender]: {
+      text: string;
+      backgroundColor: string;
+      icon: ImageSourcePropType;
+    };
+  } = {
+    [EGender.female]: {
+      backgroundColor: colors.pink,
+      text: 'Female',
+      icon: require('../../../assets/images/female.png'),
+    },
+    [EGender.male]: {
+      backgroundColor: colors.blue,
+      text: 'Male',
+      icon: require('../../../assets/images/male.png'),
+    },
+  };
+
+  const renderGenderBox = (gender: EGender) => {
+    const data = genderDictionary[gender];
+    return (
+      <View
+        style={StyleSheet.flatten([
+          styles.genderBox,
+          {
+            backgroundColor: data.backgroundColor,
+            opacity: gender === currentUser?.gender ? 1 : 0.33,
+          },
+        ])}>
+        <Image style={styles.icon} source={data.icon} />
+        <Text style={styles.whiteText}>{data.text}</Text>
+      </View>
+    );
+  };
 
   return (
     <View style={styles.mainContainer}>
@@ -35,8 +88,17 @@ const DetailsSection = () => {
         name: 'Last Name',
         value: currentUser?.name?.last ?? '',
       })}
-      {renderTextSection({name: 'Email', value: currentUser?.email ?? ''})}
-      {renderTextSection({name: 'Phone', value: currentUser?.phone ?? ''})}
+      {renderTextSection({
+        name: 'Email',
+        value: currentUser?.email ?? '',
+        icon: require('../../../assets/images/mail.png'),
+      })}
+      {renderTextSection({
+        name: 'Phone',
+        value: currentUser?.phone ?? '',
+        icon: require('../../../assets/images/ad_units.png'),
+      })}
+      {renderGenderSection()}
     </View>
   );
 };
@@ -60,9 +122,31 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginTop: 8,
     borderWidth: StyleSheet.hairlineWidth,
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   section: {
     paddingVertical: 15,
   },
   image: {width: 100, height: 100, borderRadius: 100, marginVertical: 13},
+  icon: {
+    marginEnd: 10,
+  },
+  genderBox: {
+    alignSelf: 'flex-start',
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    borderRadius: 10,
+    borderColor: colors.black,
+    borderWidth: 1,
+    marginVertical: 10,
+    marginEnd: 10,
+    flexDirection: 'row',
+  },
+  whiteText: {
+    color: colors.white,
+  },
+  genderBoxesContainer: {
+    flexDirection: 'row',
+  },
 });
