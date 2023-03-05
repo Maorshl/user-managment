369ed4a1dcc87b6c3f13fda1452d3a4a0f660bd5
Maor Shlomo <maors@moveo.co.il>
2023-03-05 08:29:51 +0200

    Changed seed text

--- userManagment/src/api/homeApi.ts
+++ userManagment/src/api/homeApi.ts
@@ -3,7 +3,7 @@ import appAxios from '../networkManager/networkManager';
 
 export const getUsersApi = async (numberOfRows: EResultsNumber) => {
   const res = await appAxios.get<{results: User[]}>(
-    `?results=${numberOfRows}&seed=maor`,
+    `?results=${numberOfRows}&seed=AirWaze`,
   );
   return res.data;
 };
